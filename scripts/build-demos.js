4700fa39758ceabd19dd23f44752f6d1d608c5c9
Mirror <mirror12k@gmail.com>
2025-11-23 07:38:31 -0500

    - renaming demo files

--- scripts/build-demos.js
+++ scripts/build-demos.js
@@ -5,57 +5,57 @@ const demos = [
   {
     name: 'Spinning Triangle',
     entry: 'demos/spinning-triangle/index.js',
-    output: 'dist/demo1.html'
+    output: 'dist/demo1-spinning-triangle.html'
   },
   {
     name: 'Mouse Triangle',
     entry: 'demos/mouse-triangle/index.js',
-    output: 'dist/demo2.html'
+    output: 'dist/demo2-mouse-triangle.html'
   },
   {
     name: 'Box Room',
     entry: 'demos/box-room/index.js',
-    output: 'dist/demo3.html'
+    output: 'dist/demo3-box-room.html'
   },
   {
     name: 'Block Game',
     entry: 'demos/block-game/index.js',
-    output: 'dist/demo4.html'
+    output: 'dist/demo4-block-game.html'
   },
   {
     name: 'Game of Life',
     entry: 'demos/game-of-life/index.js',
-    output: 'dist/demo5.html'
+    output: 'dist/demo5-game-of-life.html'
   },
   {
     name: 'Game of Life 3D',
     entry: 'demos/game-of-life-3d/index.js',
-    output: 'dist/demo6.html'
+    output: 'dist/demo6-game-of-life-3d.html'
   },
   {
     name: 'Physics Demo',
     entry: 'demos/physics-demo/index.js',
-    output: 'dist/demo7.html'
+    output: 'dist/demo7-physics-demo.html'
   },
   {
     name: 'Simplex Noise',
     entry: 'demos/simplex-noise/index.js',
-    output: 'dist/demo8.html'
+    output: 'dist/demo8-simplex-noise.html'
   },
   {
     name: 'Fisheye Lens',
     entry: 'demos/fisheye-lense-demo/index.js',
-    output: 'dist/demo9.html'
+    output: 'dist/demo9-fisheye-lense-demo.html'
   },
   {
     name: 'Metaballs',
     entry: 'demos/metaballs/index.js',
-    output: 'dist/demo10.html'
+    output: 'dist/demo10-metaballs.html'
   },
   {
     name: 'Piano',
     entry: 'demos/piano/index.js',
-    output: 'dist/demo11.html'
+    output: 'dist/demo11-piano.html'
   }
 ];
 
